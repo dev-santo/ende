c87028dc6b54c745f438b674978b9e5a378bcf02
Jociro Miguel <koktaytay@gmail.com>
2021-04-23 22:49:03 +0100

    Initial commit

---
+++ src/main/java/com/ende/ende/EndeApplication.java
@@ -0,0 +1,13 @@
+package com.ende.ende;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndeApplication.class, args);
+    }
+
+}
